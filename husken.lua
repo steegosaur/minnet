fa05db023e88f3846879776f710e38abdd29d3ae
Stæld Lakorv <staeld@staeld.co.cc>
2010-11-24 10:40:26 +0100

    separate config file, first commit

---
+++ husken.lua
@@ -0,0 +1,99 @@
+#!/usr/bin/env lua
+-- minnet.lua 0.0.7 - personal irc bot written in lua
+-- Copyright Stæld Lakorv, 2010 <staeld@staeld.co.cc>
+c   = { net = {} } -- Connection list
+owner = {
+    uname1 = "a6af798b68",
+    uname2 = "~Staeld",
+    host = "%.id%.au$"
+}
+bot = {
+    nick  = "Minnet",
+    uname = "Minnet",
+    rname = "Minnet",
+    cmdstring = "^%%",
+    nets  = {
+        { name = "furnet", addr = "eu.irc.furnet.org", c = { "#dilldall" } },
+--        { name = "scoutlink", addr = "irc.scoutlink.org", c = { "#test" } }
+    },
+    cmds  = {
+        {
+            name    = "test",
+            comment = "dummy command",
+            rep     = "Go test yourself! Sheesh."
+        },
+        {
+            name    = "quit",
+            comment = "shut down bot", -- Careful with this function!
+            rep     = "Bye.",
+            action  = function(n, u, chan, m)
+                if ( u.username == owner.uname1 ) or ( u.username == owner.uname2 ) and string.match(u.host, owner.host) then
+                    for i = 1, #c.net do
+                        c.net[i]:disconnect("Minnet quitting..")
+                    end
+                else
+                    c.net[n]:sendChat(chan, "Hey, wait a minute! I'm not taking commands from you.")
+                end
+            end
+        }
+    }
+}
+for i = 1, #bot.cmds do
+    if not bot.cmds.list then
+        bot.cmds.list = bot.cmds[i].name
+    else
+        bot.cmds.list = bot.cmds.list .. ", " .. bot.cmds[i].name
+    end
+end
+table.insert(bot.cmds,{
+    name    = "help",
+    comment = "help message with cmd list",
+    --rep     = "Lorem ipsum dolor sit helpet.",
+    action  = function(n, u, chan, m)
+        c.net[n]:sendChat(chan, name)
+        c.net[n]:sendChat(chan, "Commands: " .. bot.cmds.list)
+    end
+})
+msg = {
+    noargs = "invalid arguments specified. See --help",
+    noconf = "could not find config file",
+}
+
+-- Functions
+function err(msg, f)
+    if f then f = " " .. f else f = "" end
+    print("error: " .. msg .. f)
+    os.exit(1)
+end
+function wit(n, u, chan, m) -- Hook function for reacting to normal commands
+    if string.match(m, bot.cmdstring) then
+        m = string.gsub(m, bot.cmdstring, "")
+    end
+    if m == "" then return nil end
+    cmdFound = false
+    for i = 1, #bot.cmds do
+        if m == bot.cmds[i].name then -- Improve this for argument support!
+            print("Received command " .. m .. " from " .. u.nick .. "!" .. u.username .. "@" .. u.host .. " on " .. bot.nets[n].name .. "/" .. chan)
+            if bot.cmds[i].rep      then c.net[n]:sendChat(chan, bot.cmds[i].rep); print(os.date("%F/%T:") .. m) end
+            if bot.cmds[i].action   then bot.cmds[i].action(n, u, chan, m) end
+            cmdFound = true
+        end
+    end
+    if ( cmdFound == false ) then
+        c.net[n]:sendChat(chan, "Nevermore!")
+    end
+end
+-- Create msg.help() function
+name = ""
+io.input(arg[0])
+while not string.match(name, "^%-%-%s-(minnet%.lua.*)$") or not io.read() do
+    name = io.read()
+end
+name = string.gsub(name, "^%W*", "")
+msg.help = function()
+    print(name)
+    print("Usage: " .. arg[0] .. " [--help]")
+    os.exit()
+end
+-- }}}
+-- EOF
