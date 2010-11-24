1a1f324c1b8b37d7904279d1086d77c52b837093
Stæld Lakorv <staeld@staeld.co.cc>
2010-11-24 10:05:40 +0100

    added owner recognition for quit command; made dynamic help command; fixed empty command bug

--- minnet.lua
+++ minnet.lua
@@ -1,5 +1,5 @@
 #!/usr/bin/env lua
--- minnet.lua 0.0.5 - personal irc bot written in lua
+-- minnet.lua 0.0.7 - personal irc bot written in lua
 -- Copyright Stæld Lakorv, 2010 <staeld@staeld.co.cc>
 -- {{{ Init
 require("irc")
@@ -7,14 +7,19 @@ require("socket")
 require("ssl")
 
 c   = { net = {} } -- Connection list
+owner = {
+    uname1 = "a6af798b68",
+    uname2 = "~Staeld",
+    host = "%.id%.au$"
+}
 bot = {
     nick  = "Minnet",
     uname = "Minnet",
     rname = "Minnet",
-    cmdstring = "^#",
+    cmdstring = "^%%",
     nets  = {
-        --{ name = "furnet", addr = "eu.irc.furnet.org", c = { "#geekfurs", } },
-        { name = "scoutlink", addr = "irc.scoutlink.org", c = { "#test", } },
+        { name = "furnet", addr = "eu.irc.furnet.org", c = { "#dilldall" } },
+--        { name = "scoutlink", addr = "irc.scoutlink.org", c = { "#test" } }
     },
     cmds  = {
         {
@@ -23,20 +28,37 @@ bot = {
             rep     = "Go test yourself! Sheesh."
         },
         {
-            name    = "help",
-            comment = "help message with cmd list",
-            rep     = "Lorem ipsum dolor sit helpet."
-        },
-        {
             name    = "quit",
             comment = "shut down bot", -- Careful with this function!
             rep     = "Bye.",
-            action  = function(n)
-                c.net[n]:disconnect("Minnet quitting..")
+            action  = function(n, u, chan, m)
+                if ( u.username == owner.uname1 ) or ( u.username == owner.uname2 ) and string.match(u.host, owner.host) then
+                    for i = 1, #c.net do
+                        c.net[i]:disconnect("Minnet quitting..")
+                    end
+                else
+                    c.net[n]:sendChat(chan, "Hey, wait a minute! I'm not taking commands from you.")
+                end
             end
         }
     }
 }
+for i = 1, #bot.cmds do
+    if not bot.cmds.list then
+        bot.cmds.list = bot.cmds[i].name
+    else
+        bot.cmds.list = bot.cmds.list .. ", " .. bot.cmds[i].name
+    end
+end
+table.insert(bot.cmds,{
+    name    = "help",
+    comment = "help message with cmd list",
+    --rep     = "Lorem ipsum dolor sit helpet.",
+    action  = function(n, u, chan, m)
+        c.net[n]:sendChat(chan, name)
+        c.net[n]:sendChat(chan, "Commands: " .. bot.cmds.list)
+    end
+})
 msg = {
     noargs = "invalid arguments specified. See --help",
     noconf = "could not find config file",
@@ -48,10 +70,11 @@ function err(msg, f)
     print("error: " .. msg .. f)
     os.exit(1)
 end
-function wit(n, u, chan, m)
+function wit(n, u, chan, m) -- Hook function for reacting to normal commands
     if string.match(m, bot.cmdstring) then
         m = string.gsub(m, bot.cmdstring, "")
     end
+    if m == "" then return nil end
     cmdFound = false
     for i = 1, #bot.cmds do
         if m == bot.cmds[i].name then -- Improve this for argument support!
@@ -66,18 +89,16 @@ function wit(n, u, chan, m)
     end
 end
 -- Create msg.help() function in local scope
-do
-    local name = ""
-    io.input(arg[0])
-    while not string.match(name, "^%-%-%s-(minnet%.lua.*)$") or not io.read() do
-        name = io.read()
-    end
-    name = string.gsub(name, "^%W*", "")
-    msg.help = function()
-        print(name)
-        print("Usage: " .. arg[0] .. " [--help]")
-        os.exit()
-    end
+name = ""
+io.input(arg[0])
+while not string.match(name, "^%-%-%s-(minnet%.lua.*)$") or not io.read() do
+    name = io.read()
+end
+name = string.gsub(name, "^%W*", "")
+msg.help = function()
+    print(name)
+    print("Usage: " .. arg[0] .. " [--help]")
+    os.exit()
 end
 -- }}}
 
@@ -99,17 +120,20 @@ for i = 1, #bot.nets do
         print("Joining channel " .. bot.nets[i].c[j] .. " on " .. bot.nets[i].name)
         c.net[i]:join(bot.nets[i].c[j])
     end
+    c.net[i]:sendChat("Staeld", "Done connecting to " .. bot.nets[i].name)
+
     -- Register hooks
     c.net[i]:hook("OnChat", function(u, chan, m)
         msg = false
-        local net = i
-        if ( chan == c.net[net].nick ) then msg = true; chan = u.nick end
-        if ( msg == true ) or string.match(m, bot.cmdstring ) then wit(net, u, chan, m) end end)
+        local n = i
+        if ( chan == c.net[n].nick ) then msg = true; chan = u.nick end
+        if ( msg == true ) or string.match(m, bot.cmdstring) then wit(n, u, chan, m) end
+    end)
 end
 print("All networks connected. Awaiting commands.")
 while true do
-    for i = 1, #c.net do
-        c.net[i]:think()
+    for n = 1, #c.net do
+        c.net[n]:think()
         socket.sleep(1)
     end
 end
