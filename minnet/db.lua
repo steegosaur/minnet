98c7ffaf1f81c726c8c3b7c12a6afeb787451b61
Stæld Lakorv <staeld@staeld.co.cc>
2010-11-28 10:38:20 +0100

    working on database functions

--- minnet/db.lua
+++ minnet/db.lua
@@ -10,33 +10,68 @@ function db.error(n, u, m)
     log(m)
     c.net[n]:sendChat(u.nick, m)
 end
+function db.check(n)
+    udb:exec("INSERT INTO " .. bot.nets[n].name .. " VALUES (db_test);")
+    if ( tostring(udb:errcode()) == "1" ) then
+        udb:exec("CREATE TABLE " .. bot.nets[n].name .. " (nick,level,host,passhash,email,cur_nick);")
+        log("Created sql table " .. bot.nets[n].name)
+    end
+end
+local function forgot(n, u, f)
+    c.net[n]:sendChat(u.nick, "I couldn't use the user info; you forgot telling me the " .. f .. ".")
+end
 function db.get_user(n, nick)
     local userinfo = udb:nrows("SELECT nick,level,host,passhash,email FROM " .. bot.nets[n].name .. " WHERE cur_nick=" .. nick .. " OR nick=" .. nick)
     return userinfo
 end
 function db.add_user(n, u, nick, level, host, passhash, email)
-    if ( udb:exec("INSERT INTO " .. bot.nets[n].name .. " VALUES (" .. nick .. ", " .. level .. ", " .. host .. ", " .. passhash .. ", " .. email .. ")") ~= sqlite3.OK ) then
-        db.error(n, u, "Could not insert user info: " .. udb:errcode() .. " - " .. udb:errmsg())
-    end
-end
-function db.upd_user(n, u, nick, cur_nick, host)
-    if ( udb:exec("UPDATE " .. bot.nets[n].name .. " SET cur_nick=" .. cur_nick .. ", host=" .. host .. " WHERE nick=" .. nick) ~= sqlite3.OK ) then
-        db.error(n, u, "Could not update user info: " .. udb:errcode() .. " - " .. udb:errmsg())
-    end
-function db.set_user(u, n, nick, level, host, passhash, email, cur_nick)
-    reqs = { "nick", "level", "host", "passhash", "email" }
-    local reqsmet = true
-    for i in 1, #reqs do
-        if not _G[reqs[i]] then -- This is horrible and I should probably be shot for it, but it should work.
-            if ( reqs[i] == "passhash" ) then reqs[i] = "password" end
-            c.net[n]:sendChat(u.nick, "I couldn't update the user; you forgot telling me the " .. reqs[i] .. ".")
-            reqsmet = false
-            break
+    local cur_nick = nick
+    if     not nick     or ( nick     == "" ) then forgot(n, u, "nick")
+    elseif not level    or ( level    == "" ) then forgot(n, u, "level")
+    elseif not host     or ( host     == "" ) then forgot(n, u, "host")
+    elseif not passhash or ( passhash == "" ) then forgot(n, u, "password")
+    elseif not email    or ( email    == "" ) then forgot(n, u, "email")
+    else
+        udb:exec("SELECT * FROM " .. bot.nets[n].name .. " WHERE nick=" .. nick .. ";")
+        if ( udb:errmsg() ~= "no such column: " .. nick ) then
+            log("User already exists")
+            c.net[n]:sendChat(u.nick, "I already know that guy. Try setting the info instead.")
+            return nil
+        end
+        nick, level, host, passhash, email, cur_nick = "'" .. nick .. "'", "'" .. level .. "'", "'" .. host .. "'", "'" .. passhash .. "'", "'" .. email .. "'", "'" .. cur_nick .. "'"
+        if ( udb:exec("INSERT INTO " .. bot.nets[n].name .. " VALUES (" .. nick .. ", " .. level .. ", " .. host .. ", " .. passhash .. ", " .. email .. ", " .. cur_nick .. ")") ~= sqlite3.OK ) then
+            db.error(n, u, "Could not insert user info: " .. udb:errcode() .. " - " .. udb:errmsg())
+        else
+            log("Added user with fields nick, level, host, passhash, email (" .. nick .. ", " .. level .. ", " .. host .. ", " .. passhash .. ", " .. email .. ", " .. cur_nick .. ") on net " .. bot.nets[n].name)
+            c.net[n]:sendChat(u.nick, "I added the user.")
         end
     end
-    if ( reqsmet == true ) then
-        if ( udb:exec("UPDATE " .. bot.nets[n].name .. " SET nick=" .. nick .. ", " ..  .. ", " .. level .. ", " .. host .. ", " .. passhash .. ", " .. email .. ")") ~= sqlite3.OK ) then
+end
+function db.upd_user(n, u, cur_nick, nick, host)
+    if     not nick     or ( nick     == "" ) then forgot(n, u, "nick")
+    elseif not host     or ( host     == "" ) then forgot(n, u, "host")
+    elseif not cur_nick or ( cur_nick == "" ) then forgot(n, u, "current nick")
+    else
+        nick, host, cur_nick = "'" .. nick .. "'", "'" .. host .. "'", "'" .. cur_nick .. "'"
+        if ( udb:exec("UPDATE " .. bot.nets[n].name .. " SET cur_nick=" .. cur_nick .. ", host=" .. host .. " WHERE nick=" .. nick) ~= sqlite3.OK ) then
+            db.error(n, u, "Could not update user info: " .. udb:errcode() .. " - " .. udb:errmsg())
+        else
+            log("Updated user with fields cur_nick, host (" .. cur_nick .. ", " .. host .. ") on net " .. bot.nets[n].name)
+            c.net[n]:sendChat(u.nick, "I updated the user information.")
+        end
+    end
+end
+function db.set_user(n, u, nick, level, host, passhash, email, cur_nick)
+    if     not nick     or ( nick     == "" ) then forgot(n, u, "nick")
+    elseif not level    or ( level    == "" ) then forgot(n, u, "level")
+    elseif not host     or ( host     == "" ) then forgot(n, u, "host")
+    elseif not passhash or ( passhash == "" ) then forgot(n, u, "password")
+    elseif not email    or ( email    == "" ) then forgot(n, u, "email")
+    else
+        if ( udb:exec("UPDATE " .. bot.nets[n].name .. " SET nick=" .. nick .. ", level=" .. level .. ", host=" .. host .. ", passhash=" .. passhash .. ", email" .. email .. ")") ~= sqlite3.OK ) then
             db.error(n, u, "Could not insert user info: " .. udb:errcode() .. " - " .. udb:errmsg())
+        else
+            log("Set new fields nick, level, host, passhash, email (" .. nick .. ", " .. level .. ", " .. host .. ", " .. passhash .. ", " .. email .. ") on net " .. bot.nets[n].name)
         end
     end
 end
