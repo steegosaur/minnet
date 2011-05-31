e5203877dbdbff66e331a0596bd87fd423ce1921
Stæld Lakorv <staeld@staeld.co.cc>
2011-05-31 18:37:17 +0200

    forgot adding the new cmdvocab.lua to the git repo; updated THANKS

---
+++ minnet/cmdvocab.lua
@@ -0,0 +1,29 @@
+#!/usr/bin/env lua
+-- cmdvocab.lua - command vocabulary file for minnet
+-- Copyright Stæld Lakorv, 2011 <staeld@staeld.co.cc>
+-- This file is part of Minnet.
+-- Minnet is released under the GPLv3 - see ../COPYING
+
+-- This file is used as an abstraction layer between the received commands
+--  and the actual, known commands. This permits for looser command recognition
+--  while avoiding duplicate functions.
+--  The actual implementation is still subject to change.
+
+-- Syntax: funcname = { "pattern%s+matching%s+the%s+said%s+command$" }
+
+bot.commands = {
+    uptime  = { "how%s+long", "for%s+how%s+long", "what'?s?%s+.*uptime.*" },
+    be      = { "be" },
+    join    = { "join", "go%s+to" },
+    part    = { "part", "get%s+out" },
+    quit    = { "get%s+off", "shut%s+down", "quit" },
+    reload  = { "reload" },
+    set     = { "set" },
+    load    = { "load" },
+    reseed  = { "reseed", "reset%s+the%s+crypto.-seed" },
+    say     = { "say" },
+    version = { "version", "ctcp%s+version", "ask%s+for%s+.*version.*of" },
+    identify= { "identify", "i'?%s-a?m" },
+    db      = { "db", "database" },
+}
+-- EOF
