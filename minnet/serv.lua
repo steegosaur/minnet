94ca2e98930ca725c631ac27ed893921c53fa5c6
Stæld Lakorv <staeld@staeld.co.cc>
2011-11-01 10:37:24 +0100

    forked for Services functionality

---
+++ minnet/serv.lua
@@ -0,0 +1,9 @@
+#!/usr/bin/env lua
+-- serv.lua - service functions for minnet
+-- Copyright Stæld Lakorv, 2010-2011 <staeld@staeld.co.cc>
+-- This file is part of Minnet.
+-- Minnet is released under the GPLv3 - see ../COPYING
+
+-- This file will provide basic Services functionality to Minnet
+--+ including nick protection
+
